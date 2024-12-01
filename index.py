2d1a87e511927a260041092da1cfa1e3d73aa294
Michael Smith <michael.smith.ok@gmail.com>
2024-12-01 11:40:31 -0600

    Square image and prepare for use

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
-import os
 import io
-import tempfile
+from os import environ
 
 from model import super_resolution_png
 
@@ -8,11 +7,14 @@ from PIL import Image
 from flask import Flask, flash, request, redirect, send_file
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = tempfile.gettempdir()
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+BLACK = (0,0,0)
+RESOLUTION_LIMIT = 2500
+if "RESOLUTION_LIMIT" in environ:
+  RESOLUTION_LIMIT = int(environ["RESOLUTION_LIMIT"])
+print("Resolution limit: {}".format(RESOLUTION_LIMIT))
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 
@@ -34,6 +36,8 @@ def upload_file():
       filename = secure_filename(file.filename)
       sr_bytes_png = None
       buf = io.BytesIO(file.read())
+
+      # Convert to png if necessary
       if extension != 'png':
         print("Converting to png")
         jpgimg = Image.open(buf)
@@ -45,10 +49,37 @@ def upload_file():
         jpgimg.save(pngbuf, format='PNG')
         print("saved")
         buf = pngbuf
+
+      # Make image a square
+      print("Squaring")
+      img = Image.open(buf)
+      width, height = img.size
+      ratio = width / height
+      max_dimension = max(width, height)
+      if max_dimension > RESOLUTION_LIMIT:
+        return "", 413 # too large
+      tmpimg = Image.new(img.mode, (max_dimension, max_dimension), BLACK)
+      tmpimg.paste(img, (0, 0))
+      img = tmpimg
+      img.save(buf, format="PNG")
+      print("Squared")
+
+      print("Super rezing")
       sr_bytes_png = super_resolution_png(buf).getvalue()
       print("Super resolution done")
+      buf = io.BytesIO(sr_bytes_png)
+
+      # Crop out from the expanded image
+      img = Image.open(buf)
+      width, height = img.size
+      if ratio >= 1.0:
+        img = img.crop((0, 0, width, int(height / ratio)))
+      else:
+        img = img.crop((0, 0, (width * ratio), height))
+      img.save(buf)
+
       return send_file(
-        io.BytesIO(sr_bytes_png),
+        buf,
         mimetype='image/png',
         as_attachment=False,
         download_name=filename)
